be58047e35908ad21f9a53ada1c5a14843cf0047
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-10-14 17:21:40 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+//---------------------------------------------------------------------------------------------------------------
+
+
+//https://learn.microsoft.com/en-us/training/paths/develop-ai-agents-azure-open-ai-semantic-kernel-sdk/
+
+
+//---------------------------------------------------------------------------------------------------------------
+
+
+using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder();
+
+builder.AddAzureOpenAIChatCompletion(
+    "your-deployment-name",
+    "your-endpoint",
+    "your-api-key",
+    "deployment-model");
+
+var kernel = builder.Build();
+
+var result = await kernel.InvokePromptAsync("Give me a list of breakfast foods with eggs and cheese");
+
+Console.WriteLine(result);
